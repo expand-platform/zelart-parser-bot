373a5f36445164ad34b8f5b4bc718d0d393466ce
Ca-tt <damir96.lukyanenko@gmail.com>
2025-05-21 23:14:24 +0300

    current hour

--- src/bot/bot_init.py
+++ src/bot/bot_init.py
@@ -1,14 +1,20 @@
 from src.bot.bot_handlers import Bot
 from dotenv import load_dotenv
-import os
+from datetime import datetime
+from os import environ
+
+current_hour = datetime.now().hour
+print("🐍 current_hour is:", current_hour)
 
 
 def start_bot():
     print(f"✅ Bot launched!") 
-    environment = os.environ["ENVIRONMENT"]
+    environment = environ["ENVIRONMENT"]
+
     if environment == "PRODUCTION":
         print("🌐  Bot is running in production mode")
         Bot().bot.infinity_polling()
+
     elif environment == "DEVELOPMENT":
         print("⚙️  Bot is running in development mode")
         Bot().bot.infinity_polling(restart_on_change=True)
